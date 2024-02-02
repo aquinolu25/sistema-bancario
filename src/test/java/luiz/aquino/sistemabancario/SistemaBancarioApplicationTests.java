8f0c0d3b782d9eea50c9c9e2e984a28c00ce0215
Luiz Aquino <quinnpw@live.com>
2024-02-02 17:13:32 -0300

    Initial project with database config

---
+++ src/test/java/luiz/aquino/sistemabancario/SistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package luiz.aquino.sistemabancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
